c227d06be0bfed494c5a5088de1d3700a247fc58
ishimoto <enoshima@Miyalab-Mac-Pro.local>
2016-06-25 12:45:51 +0900

    Add various models

--- holecard_handicapper/model/handicapper_fitting/2_layer/data_reader.py
+++
@@ -1,4 +0,0 @@
-from learning_data import data_reader
-
-def load_data(file_path, test_ratio):
-  return data_reader.load_data(file_path, test_ratio)
---
+++ holecard_handicapper/model/handicapper_fitting/5_layer/5_layer_node_60/data_reader.py
@@ -0,0 +1,4 @@
+from learning_data import data_reader
+
+def load_data(file_path, test_ratio):
+  return data_reader.load_data(file_path, test_ratio)
